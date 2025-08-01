c185c46621ee7b3636316313e10e065eb5d81a47
albecabrera <albe.cabrera@yahoo.de>
2025-08-01 11:00:34 +0200

    variables primitivas

---
+++ src/variablesprimitivas/Main.java
@@ -0,0 +1,24 @@
+package variablesprimitivas;
+
+public class Main {
+    public static void main(String[] args) {
+        byte age = 23; // admite un valor hasta 127
+        System.out.println("age = " + age);
+        double price = 19.99f;
+        int xAxis, yAxis;
+        xAxis = 10;
+        yAxis = 20;
+        age=30;
+        System.out.println("age = " + age);
+
+        System.out.println("xAxis = " + xAxis);
+        System.out.println("yAxis = " + yAxis);
+
+        char initial = 'G';
+        System.out.println("initial = " + initial);
+
+        boolean isTeacher = true;
+        
+
+    }
+}
