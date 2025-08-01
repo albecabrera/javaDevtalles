d1d50f895d6070a10df9b6f31945ae009b214374
albecabrera <albe.cabrera@yahoo.de>
2025-08-02 00:21:57 +0200

    bucles ejercicios

---
+++ src/bucles/BucleEjercicio.java
@@ -0,0 +1,11 @@
+package bucles;
+
+public class BucleEjercicio {
+    public static void main(String[] args) {
+
+        for (int i = 6; i <= 10; i++) {
+            System.out.println("i = " + i);
+
+        }
+    }
+}
---
+++ src/condicionales/IfCondicional2.java
@@ -0,0 +1,13 @@
+package condicionales;
+
+public class IfCondicional2 {
+    public static void main(String[] args) {
+        String name = "Alfredo";
+
+        if (name == "Alberto") {
+            System.out.println("name = " + name);
+        }else {
+            System.out.println("No puedes pasar");
+        }
+    }
+}
