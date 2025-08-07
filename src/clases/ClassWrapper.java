2dadadc456a6fdc7ae4e31ecd7a89b3a6361df68
albecabrera <albe.cabrera@yahoo.de>
2025-08-07 10:16:12 +0200

    clase wrapper

---
+++ src/clases/ClassWrapper.java
@@ -0,0 +1,10 @@
+package clases;
+
+public class ClassWrapper {
+    public static void main(String[] args) {
+      int number = 10;  // Dato primitivo
+      Integer number2 = Integer.valueOf(30); // Clase
+      Integer number3 = 40;
+        System.out.println(number2);
+    }
+}
