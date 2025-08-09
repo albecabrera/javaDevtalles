085fbff2ce1e564c516247148433252f68955f8e
albecabrera <albe.cabrera@yahoo.de>
2025-08-09 09:23:22 +0200

    métodos

---
+++ src/metodos/Metodos.java
@@ -0,0 +1,13 @@
+package metodos;
+
+public class Metodos {
+    public static void main(String[] args) {
+        //     método length()
+        String text = "Hola, mundo!";
+        System.out.println(text.length()); // 12
+        // método chartAt(int index)
+        System.out.println("Carácter en la posición 2: " + text.charAt(2)); // l
+
+    }
+
+}
--- src/scannerExample/SimpleCalculator.java
+++ src/scannerExample/SimpleCalculator.java
@@ -4,20 +4,35 @@ import java.util.Scanner;
 
 public class SimpleCalculator {
     public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in); // La clase Scanner en Java se utiliza para leer datos de
-    // entrada, generalmente desde el teclado (es decir, desde la consola).
-        System.out.println("Ingrese el primer número");
-    int firstNumber = sc.nextInt();
+        Scanner scanner = new Scanner(System.in); // La clase Scanner en Java se utiliza para leer datos de
+        // entrada, generalmente desde el teclado (es decir, desde la consola).
 
-        System.out.println("Ingrese el segundo número");
-    int secondNumber = sc.nextInt();
-    int difference = firstNumber - secondNumber;
+        int first = getNumber(scanner, "Ingrese el primer número");
+        int second = getNumber(scanner, "Ingrese el segundo número");
 
-    int sum = firstNumber + secondNumber;
-        System.out.println("La suma de a + b = " + (sum));
-        System.out.println("La diferencia de a - b = " + (difference));
+        printArithmeticOperations(first, second);
 
-}
+        compareNumber(first, second);
+
+
+    }
+    public static int getNumber(Scanner scanner, String message) {
+        System.out.println(message);
+        int number = scanner.nextInt();
+        return number;
+    }
+
+    public static void printArithmeticOperations(int firstNumber, int secondNumber){
+        System.out.println("La suma de a + b = " + (firstNumber + secondNumber));
+        System.out.println("La diferencia de a - b = " + (firstNumber - secondNumber));
+    }
+    public static void compareNumber(int first, int second){
+        if (first > second) {
+            System.out.println("El primer número es mayor");
+        } else {
+            System.out.println("El segundo número es mayor o igual");
+        }
+    }
 }
 
 
