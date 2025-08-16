9e0b50d5b9589aaa1a9c0a2e3416d4ec80053b9b
albecabrera <albe.cabrera@yahoo.de>
2025-08-16 10:28:47 +0200

    otros cambios

---
+++ src/POO/Engine.java
@@ -0,0 +1,7 @@
+package POO;
+
+public class Engine {
+    public void start(){
+        System.out.println("motor encendido");
+    }
+}
--- src/POO/Vehicle.java
+++ src/POO/Vehicle.java
@@ -10,16 +10,31 @@ public class Vehicle {
         this.model = model;
         this.year = year;
     }
+    package POO;
 
-    public void start(){
-        System.out.println("Encendido el vehiculo ");
-    }
+    public class Main {
+        public static void main(String[] args) {
 
-    public void start(boolean silentMode) {
-        if (silentMode){
-        System.out.println("Está encendido en modo silencioso");
-        }else{
-            System.out.println("Encendido el vehículo");
+        /*Vehicle vehicle = new Vehicle("Fiat", "Duna", 2000);
+                vehicle.setYear(2020);
+
+        System.out.println(vehicle.toString());*/
+
+            Vehicle myCar = new Car("Toyota", "Corolla", 2024, 4);
+            Vehicle motorcycle = new Motorcycle("Honda", "CBR", 2021, false);
+
+            myCar.start();
+            motorcycle.start();
+
+            printVehicle(motorcycle);
+            printVehicle(myCar);
+
+            ((Car)myCar).setDoors(5);
+        }
+
+        public static void printVehicle(Vehicle vehicle){
+            System.out.println(vehicle.toString());
+
         }
     }
 
